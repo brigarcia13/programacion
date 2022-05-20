16814d02a8a8acf3d0c9ca7d1ef87edcd85d67fa
brigarcia13 <britney.garcia.arroyo@utelvt.edu.ec>
2022-05-20 14:44:57 -0500

    actualizacion

--- ACTIVIDAD-B2-C2/GarciaArroyoBritney/GarciaBritney-CuentaMoneda.cpp
+++ ACTIVIDAD-B2-C2/GarciaArroyoBritney/GarciaBritney-CuentaMoneda.cpp
@@ -1,30 +1,46 @@
+//====> Nombre del programa:  contador de monedas
+//====> autora= Garcia Britney                   
+//====> Archivo: cuentamoneda.cpp
+//====> Fecha de elaboracion: 2022-05-03 
+//====> fecha ultima actualizacion: 2022-05-20
+
+
 #include<iostream>
 using namespace std;
 int main()
 {
-int num,c=0,c1=0, c2=0;
-float x, t=0,t1=0,t2=0,mn=0.10;
-cout<<"ingrese la cantidad de monedas: "; cin>>num;
+int ga_num,ga_c=0,ga_c1=0, ga_c2=0;
+float ga_x, ga_t=0,ga_t1=0,ga_t2=0,ga_mn=0.10;
+cout<<"ingrese la cantidad de monedas: "; cin>>ga_num;
 do
 {
-	cout<<"ingrese el valor de la mameda(0.10,0.25): "; cin>>x;
-c=c+1;
-t=t+x;
-if (x==mn)
+	cout<<"ingrese el valor de la mameda(0.10,0.25): "; cin>>ga_x;
+ga_c=ga_c+1;
+ga_t=ga_t+ga_x;
+if (ga_x==ga_mn)
 {
-	c1=c1+1;
-	t1=t1+x;
+	ga_c1=ga_c1+1;
+	ga_t1=ga_t1+ga_x;
 }
-else if(x==0.25)
+else if(ga_x==0.25)
 {
-	c2=c2+1;
-	t2=t2+x;
+	ga_c2=ga_c2+1;
+	ga_t2=ga_t2+ga_x;
 }
 }
-while(c<num);
-	cout<<"el numero de monedas de 0.10ctvs ingresadas es de: "<<c1<<endl;
-	cout<<"el valor total de monedas de 0.10ctvs es: "<<t1<<endl;
-	cout<<"el numero de monedas de 0.25ctvs es: "<<t2<<endl;
-	cout<<"el valor total de monedas de 0.25ctvs es:"<<t2<<endl;
+while(ga_c<ga_num);
+cout<<"//====> Nombre del programa:  contador de moneda"<<endl;
+cout<<"//====> autora= Garcia Britney"<<endl;
+cout<<"//====> Archivo: cuentamoneda.cpp"<<endl;
+cout<<"//====> Fecha de elaboracion: 2022-05-03"<<endl; 
+cout<<"//====> fecha ultima actualizacion: 2022-05-20"<<endl;
+
+
+
+
+	cout<<"el numero de monedas de 0.10ctvs ingresadas es de: "<<ga_c1<<endl;
+	cout<<"el valor total de monedas de 0.10ctvs es: "<<ga_t1<<endl;
+	cout<<"el numero de monedas de 0.25ctvs es: "<<ga_t2<<endl;
+	cout<<"el valor total de monedas de 0.25ctvs es:"<<ga_t2<<endl;
 	return 0;
 }
