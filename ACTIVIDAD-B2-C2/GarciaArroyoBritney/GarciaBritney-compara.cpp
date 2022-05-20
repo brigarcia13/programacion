064bc366954686c9255e183aff4ca328c2368935
brigarcia13 <britney.garcia.arroyo@utelvt.edu.ec>
2022-05-20 14:31:49 -0500

    actualizacion

--- ACTIVIDAD-B2-C2/GarciaArroyoBritney/GarciaBritney-compara.cpp
+++ ACTIVIDAD-B2-C2/GarciaArroyoBritney/GarciaBritney-compara.cpp
@@ -1,21 +1,35 @@
+////====> Nombre del programa: calcular la edad   
+////====> autora= Garcia Britney                   
+////====> Archivo: laedad.cpp
+////====> Fecha de elaboracion: 2022-05-03 
+////====> fecha ultima actualizacion: 2022-05-20
+
 #include<iostream>
 using namespace std;
 int main()
 {
-	float x,y;
-	cout<<" ingrese x: ";cin>>x;
-	cout<<" ingrese y: ";cin>>y;
+	float ga_x,ga_y;
+	cout<<" ingrese x: ";cin>>ga_x;
+	cout<<" ingrese y: ";cin>>ga_y;
 
 
-	if(x==y){
-	cout<<"el valor de x= "<<x<<" es igual a y= "<<y<<endl;
+	if(ga_x==ga_y){
+
+cout<<"//====> Nombre del programa: compara dos nuneros"<<endl;   
+cout<<"//====> autora= Garcia Britney"<<endl;           
+cout<<"//====> Archivo: compara.cpp"<<endl;
+cout<<"//====> Fecha de elaboracion: 2022-05-03"<<endl;
+cout<<"//====> fecha ultima actualizacion: 2022-05-20"<<endl;
+
+
+	cout<<"el valor de x= "<<ga_x<<" es igual a y= "<<ga_y<<endl;
 
 	}else{
-if (x<y) {
-	cout<<" el valor de x= "<<x<<" es menor a y= "<<y<<endl;
+if (ga_x<ga_y) {
+	cout<<" el valor de x= "<<ga_x<<" es menor a y= "<<ga_y<<endl;
 
 	}else{
-	cout<<" el valor de y= "<<y<<" es menor a x= "<<x<<endl;
+	cout<<" el valor de y= "<<ga_y<<" es menor a x= "<<ga_x<<endl;
 	}
 
 	}
