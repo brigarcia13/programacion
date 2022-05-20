a2718fb5e74f426ef12a524c5d94f3871b33e104
brigarcia13 <britney.garcia.arroyo@utelvt.edu.ec>
2022-05-20 12:38:38 -0500

    actualizacion

--- ACTIVIDAD-B2/GarciaArroyoBritney/GarciaBritney-sumaN.cpp
+++ ACTIVIDAD-B2/GarciaArroyoBritney/GarciaBritney-sumaN.cpp
@@ -1,17 +1,31 @@
+//====> Nombre del programa: suma de dos numeros
+//====> autora= Garcia Britney
+//====> Archivo: suma.cpp
+//====> Fecha de elaboracion: 2022-05-03
+//====> fecha ultima actualizacion: 2022-05-20
+
 #include<iostream>
 using namespace std;
 int main()
 {
-	float n,cv,c=0,t=0;
-	cout<<"ingrese la cantidad se valores a sumar: ";cin>>cv;
+	float GA_n,GA_cv,GA_c=0,GA_t=0;
+	cout<<"ingrese la cantidad se valores a sumar: ";cin>>GA_cv;
 	do
 	{
-		cout<<"ingrese los valores: "; cin>>n;
-		c=c+1;
-		t=t+n;
+		cout<<"ingrese los valores: "; cin>>GA_n;
+		GA_c=GA_c+1;
+		GA_t=GA_t+GA_n;
 	}
-	while(c<cv);
-	cout<<"el resultado de la suma es: "<<t<<endl;
+	while(GA_c<GA_cv);
+cout<<"//====> Nombre del programa: suma de dos numeros"<<endl;
+cout<<"//====> autora= Garcia Britney"<<endl;
+cout<<"//====> Archivo: suma.cpp"<<endl;
+cout<<"//====> Fecha de elaboracion: 2022-05-03"<<endl;
+cout<<"//====> fecha ultima actualizacion: 2022-05-20"<<endl;
+	
+
+
+	cout<<"el resultado de la suma es: "<<GA_t<<endl;
 	return 0;
 
 } 
